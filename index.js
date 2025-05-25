43dce4d03c233d6b09f7ced92b9490dc1b0df764
Mireau-Jules <saintlouismireaujules@gmail.com>
2025-05-24 20:16:22 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let messages =[];
+    for (let i = 0; i<name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    
+    return messages;
+}
+
+function countDown (number){
+    for (let i=number; i>=0; i--){
+        console.log(i);
+    }
+}
